afdd9978397d721008205a023b343e576def3713
Sávio Raires <savioraires10@gmail.com>
2021-07-17 13:55:31 -0300

    Corringindo testes

--- src/test/kotlin/com/github/savitoh/bytebank/contas/ContaTest.kt
+++ src/test/kotlin/com/github/savitoh/bytebank/contas/ContaTest.kt
@@ -8,15 +8,15 @@ internal class ContaTest {
 
     @Test
     fun deve_retornar_quantidade_contas_criadas() {
-        val titularContaSalario = Cliente(nome = "Sávio", cpf = "111.111.111-11", senha = "123")
-        val contaSalario = ContaSalario(titular = titularContaSalario, numero = 1)
-        val titularContaPoupanca = Cliente(nome = "Fernanda", cpf = "111.111.111-11", senha = "234")
-        val contaPoupanca = ContaPoupanca(titular = titularContaPoupanca, numero = 1)
-        val titularContaCorrente = Cliente(nome = "Leticia", cpf = "111.111.111-11", senha = "345")
-        val contaCorrente = ContaCorrente(titular = titularContaCorrente, numero = 1)
+        val  quantidadeContaCriadas = Conta.total
 
-        assertEquals(3, contaSalario.getTotal())
-        assertEquals(3, contaPoupanca.getTotal())
-        assertEquals(3, contaCorrente.getTotal())
+        val titularContaSalario = Cliente(nome = "Sávio", cpf = "111.111.111-11", senha = "123")
+        ContaSalario(titular = titularContaSalario, numero = 1)
+        val titularContaPoupanca = Cliente(nome = "Fernanda", cpf = "111.111.111-11", senha = "234")
+        ContaPoupanca(titular = titularContaPoupanca, numero = 1)
+        val titularContaCorrente = Cliente(nome = "Leticia", cpf = "111.111.111-11", senha = "345")
+        ContaCorrente(titular = titularContaCorrente, numero = 1)
+
+        assertEquals(quantidadeContaCriadas+3, Conta.total)
     }
 }
