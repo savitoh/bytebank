5c47f09d8c7c47bdcdce2c12fd2eedd66dd2cc62
Sávio Raires <savioraires10@gmail.com>
2021-07-18 20:52:18 -0300

    Adicionando modelo: Endereco

--- src/main/kotlin/com/github/savitoh/bytebank/clientes/Cliente.kt
+++ src/main/kotlin/com/github/savitoh/bytebank/clientes/Cliente.kt
@@ -5,7 +5,8 @@ import com.github.savitoh.bytebank.auth.Autenticavel
 class Cliente(
     private val nome: String,
     private val cpf: String,
-    private val senha: String
+    private val senha: String,
+    private val endereco: Endereco? = null
 ) : Autenticavel {
 
     override fun autentica(senha: String) = senha == this.senha
